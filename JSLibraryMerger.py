f31735891460406088d31620dc44fca32112adbe
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-02-08 11:58:40 +0200

    Add a library to the array

--- JSLibraryMerger.py
+++ JSLibraryMerger.py
@@ -16,6 +16,7 @@ def main():
         "highstock.js",
         "highstock-exporting.js",
         "highmaps-module.js",
+		"data-module.js",
         "treemap-module.js",
         "highcharts-more.js",
         "highcharts-drilldown.js",
